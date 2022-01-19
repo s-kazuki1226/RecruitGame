d2f506a0f15a62e3bb89dab16c0d1317cff40cc9
kazuki.shida <kazuki.shida1226@gmail.com>
2022-01-19 15:59:03 +0000

    add precautions

---
+++ db/migrate/20220119153431_add_ip_to_users.rb
@@ -0,0 +1,5 @@
+class AddIpToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :ip, :string, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_085207) do
+ActiveRecord::Schema.define(version: 2022_01_19_154308) do
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "model"
